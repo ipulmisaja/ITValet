38293e5b6d4f942f0abcbd7f1b57ea0bf6b3e97b
Syaifur Rijal <syaifur.rijal@hotmail.com>
2024-07-05 15:13:02 +0800

    Edit tooltip tombol

--- resources/views/components/pages/cell-button/delete.blade.php
+++ resources/views/components/pages/cell-button/delete.blade.php
@@ -1,5 +1,5 @@
 {{-- Delete Button Inside Table --}}
-<button type="button" wire:click="deleteItem('{{ $id }}')" x-data x-tooltip.raw="{{ $label }}"
+<button type="button" wire:click="deleteItem('{{ $id }}')" x-data x-tooltip.raw="Hapus Baris"
     class="text-red-500 hover:text-red-600">
     <x-icons.herosolid name="trash" class="h-5 w-5" />
 </button>
